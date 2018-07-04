951028771a403d8e28d89fc5154a43eca702c1f6
leidy <leidyzulu9@gmail.com>
2018-07-04 17:55:28 -0500

    Se implementa porgreesbar, cap 8

--- src/main/java/app/com/kotlinatc/Main2Activity.kt
+++ src/main/java/app/com/kotlinatc/Main2Activity.kt
@@ -5,16 +5,47 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
+
+    var progressBarStatus = 0
+    var rate = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        buttonSave.setOnClickListener{
+            Thread(Runnable {
+                while (progressBarStatus < 100){
+                    try {
+                    rate += 10
+                        Thread.sleep(500)
+
+                    }catch (e: InterruptedException){
+                        e.printStackTrace()
+                    }
+                    progressBarStatus = rate
+                    progressBar.progress = progressBarStatus
+                }
+                runOnUiThread(Runnable { progressBar.visibility =ProgressBar.GONE })
+
+
+
+            }).start()
+        }
     }
 
+
+
     fun save(view: View){
+        //showAlertDialog()
+
+    }
+
+    fun showAlertDialog(){
         val alert = AlertDialog.Builder(this)
         alert.setTitle("save")
         alert.setMessage("Desea guardar los cambios?")
